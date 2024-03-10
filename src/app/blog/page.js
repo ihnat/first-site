29cd05569a40ae4cc031fac7273554a1c582decd
Ihnat <ihnat.kryshchyk@thirdfin.co.uk>
2024-03-10 12:15:39 +0100

    First commit

---
+++ src/app/blog/page.js
@@ -0,0 +1,23 @@
+
+export default function Blog() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <h1>
+          Blog Women in Tech
+        </h1>
+
+        <div>
+          <h3>
+            Запущен новый курс по Next.js в рамках нашего проекта
+          </h3>
+        </div>
+        <div>
+          <h3>
+            Отзывы на next.js курс великолепные!
+          </h3>
+        </div>
+      </div>
+    </main>
+  );
+}
